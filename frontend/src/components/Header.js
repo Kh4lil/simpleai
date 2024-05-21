1be3c9d65cc7f663a01b25f39c0578c4b6db0973
Khalil <khalil.oumimoun@gmail.com>
2024-05-20 21:20:35 -0500

    header, hero, features section

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../styles/Header.css";
+import logo from "../assets/images/logo.png";
+
+const Header = () => {
+  return (
+    <header className="navbar navbar-expand-lg navbar-dark" role="banner">
+      <div className="container">
+        <div className="navbar-brand">
+          <a href="/">
+            <img src={logo} height="30" width="130" alt="Stratus" />
+          </a>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#pages">
+                Pages
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#features">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#blog">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#shop">
+                Shop
+              </a>
+            </li>
+            <li className="nav-item th-accent">
+              <a
+                className="nav-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://1.envato.market/stratus"
+              >
+                Purchase
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/cart">
+                <i className="fas fa-shopping-cart"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
