37f6c3397c2c63d03e590ecbba93d9cad5a1f049
Khalil <khalil.oumimoun@gmail.com>
2024-05-23 12:05:52 -0500

    Added Footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,22 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      <div className="footer-container">
+        <p className="footer-copyright">Copyright © 2022</p>
+        <div className="footer-social-icons">
+          <a href="#discord" className="footer-icon">
+            <i className="fab fa-discord"></i>
+          </a>
+          <a href="#telegram" className="footer-icon">
+            <i className="fab fa-telegram-plane"></i>
+          </a>
+          <a href="#twitter" className="footer-icon">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#youtube" className="footer-icon">
+            <i className="fab fa-youtube"></i>
+          </a>
+        </div>
       </div>
     </footer>
   );
