4069b021cf1ca6c1708586094c612acf203fc26d
Khalil <khalil.oumimoun@gmail.com>
2024-05-24 21:13:09 -0500

    added ourApproachsection

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutUsHero from "../components/AboutUsHero";
 import "../styles/AboutUsHero.css";
+import OurApproachSection from "../components/OurApproachSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutUsHero />
+      <OurApproachSection />
     </>
   );
 };
