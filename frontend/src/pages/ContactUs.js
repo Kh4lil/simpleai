e73a289fa02f0fbfd0690cf8079fa62320442328
Khalil <khalil.oumimoun@gmail.com>
2024-05-24 23:54:11 -0500

    contactus page

---
+++ frontend/src/pages/ContactUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactUsDetailsSection from "../components/ContactUsDetailsSection";
+import CallToActionSection from "../components/CallToActionSection";
+
+const ContactUs = () => {
+  return (
+    <div>
+      <ContactUsDetailsSection />
+      <CallToActionSection />
+    </div>
+  );
+};
+
+export default ContactUs;
