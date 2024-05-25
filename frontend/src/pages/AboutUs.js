676cc6f420f1c1712dd50eb8cc85a31863c1271c
Khalil <khalil.oumimoun@gmail.com>
2024-05-24 21:40:14 -0500

    our secret section

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 import AboutUsHero from "../components/AboutUsHero";
 import "../styles/AboutUsHero.css";
 import OurApproachSection from "../components/OurApproachSection";
+import WhatsOurSecretSection from "../components/WhatsOurSecretSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutUsHero />
       <OurApproachSection />
+      <WhatsOurSecretSection />
     </>
   );
 };
