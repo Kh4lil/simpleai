a18602f4cb6efa7329495586ad2dc1bf5eddfef9
Khalil <khalil.oumimoun@gmail.com>
2024-05-28 19:03:44 -0500

    perks

--- frontend/src/pages/Careers.js
+++ frontend/src/pages/Careers.js
@@ -2,12 +2,14 @@ import React from "react";
 import CareerHero from "../components/CareersHero";
 import "../styles/CareersHero.css";
 import CareerImageBannerSection from "../components/CareerImageBannerSection";
+import PerksAndBenefits from "../components/PerksAndBenefits";
 
 const Careers = () => {
   return (
     <>
       <CareerHero />
       <CareerImageBannerSection />
+      <PerksAndBenefits />
     </>
   );
 };
