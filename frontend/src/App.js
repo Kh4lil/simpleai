8db4e51990eeee140774b8a0e209fe57eeb91ff3
Khalil <khalil.oumimoun@gmail.com>
2024-06-22 16:54:32 -0500

    Fix paths and routing issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Header from "./components/Header";
